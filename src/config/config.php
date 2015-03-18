9892fdac300423d118e71c6de0eac98f6b5f6b54
ViorelRadusi <ViorelRadusi@users.noreply.github.com>
2015-03-18 09:38:19 +0200

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -8,10 +8,10 @@ return [
     'certificates_path' =>  __DIR__.'/../certificates/', // directory which holds 'public.cer' and 'private.key'
     'public_cer'  =>  'public.cer',
     'private_key' =>  'private.key',
-    'signature'   =>  '1YBV-4L5L-G9VH-UF18-526U', // 'XXXX-XXXX-XXXX-XXXX-XXXX',
+    'signature'   =>  'XXXX-XXXX-XXXX-XXXX-XXXX', // Signature from mobilpay
     'confirm_url' =>  'mobilpay/confirm',
     'return_url'  =>  '/',
     'model'       =>  'Ticket', //Model to update whene the confirm_url returns the response
-    'status'      =>  'status', // Fielmd in the model that will be updated acording to the returned response
+    'status'      =>  'status', // Field in the model that will be updated acording to the returned response
   ]
 ];
